79a92112af29418d9af478e65c4ce850cf98a496
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-02-05 22:46:27 +0530

    datatypes in javascript

---
+++ 01basics/02_datatypess.js
@@ -0,0 +1,21 @@
+"use strict"; //treat  all js code as newer version
+//alert (3+3)---> we are using node.js,not browser.
+
+let myname = "Rishabh" //string datatype
+let age = 20 // integer
+let isLoggedIn = false //boolean
+let state = null 
+
+//number => 2 ^53
+//bigint
+//string = ""
+//boolean => true/false
+//null=>  stand alone value
+//undefined=>
+//symbol=>unique things to store
+console.table([myname,age,isLoggedIn,state])
+console.log(typeof myname)
+console.log(typeof null)//interview ques->object
+console.log(typeof undefined)//interview question->undefined
+
+//objects
