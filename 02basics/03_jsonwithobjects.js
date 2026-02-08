3a353aa3fa74e76d1a2936e972d48d383fe9ee7a
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-02-08 23:07:17 +0530

    json with objects

---
+++ 02basics/03_jsonwithobjects.js
@@ -0,0 +1,21 @@
+//json objects
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+//objects e
+const {courseInstructor:instruct} = course
+console.log(instruct)
+//API as objects
+// {
+//    "name": "rishabh",
+//     "Coursename": "js in hindi",
+//     "price": "free"
+// }
+//json --> javascript object notation
+//as arrays
+[
+    {},
+    {}
+]
