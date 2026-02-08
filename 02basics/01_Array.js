3dcd9472a3a31132a8b12f537448ce1ce7ffdbbe
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-02-08 20:40:01 +0530

    array in javascript

---
+++ 02basics/01_Array.js
@@ -0,0 +1,57 @@
+//Array
+const myArr = [1,2,4,5,7,0]//array can be resizable
+//shallow copy krta h
+const myHeroes = ["shaktiman","naagraj"]
+
+const myArr2 = new Array(1,3,4,5)
+// console.log(myArr2[1]);
+
+// Array methods
+myArr.push(6)
+//console.log(myArr)
+//myArr.pop();
+
+myArr.unshift(9)
+//console.log(myArr)
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(0));
+
+const newArr = myArr.join()
+//  console.log(myArr)
+//  console.log(newArr)
+
+//slice and splice
+// console.log("A ",myArr);
+
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+
+// console.log("B ",myArr);
+
+// const myn2 = myArr.splice(1,3);
+// console.log("C ",myArr);
+// console.log(myn2)
+
+//**********************Array part 2 */
+
+const marvel_heros = ["thor","Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros)
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+//console.log(all_new_heros)
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity)
+//console.log(real_another_array);
+
+// console.log(Array.isArray("Rishabh"))
+// console.log(Array.from("Rishabh"))
+// console.log(Array.from({name: "Rishabh"}))//interesting
+
