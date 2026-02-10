562c66feccdb151120ad57aa834f4accb29f02a8
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-02-10 23:12:01 +0530

    conrolflow in javascript

---
+++ 03advance/05_controlflow.js
@@ -0,0 +1,34 @@
+//if 
+// if(condition){//condition shoul be true to run if condition
+
+// }
+
+// == //this does  not check the typesof data
+// === ---> this operator also checks the data and its types
+ const isUserloggedIn =  true
+
+//  if(2 == "2"){
+//     console.log("executed");
+//  }
+ 
+ 
+//  if(2 === "2"){
+//     console.log("executed");
+//  }
+//  else{
+//     console.log("Rishabh")
+//  }
+
+//console.log(2 =="2")
+
+// const score = 200;
+
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);//this line can not be run outside the if condition
+//}
+
+ const balance = 1000;
+
+// if(balance>500) console.log("test"),console.log("test2")---> not a good practice avoid to write this type of code
+
